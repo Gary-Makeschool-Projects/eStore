612d7e38b50da478f67f8a49321e6d595950b4a9
imthaghost <imthaghost@protonmail.com>
2019-10-04 05:25:45 -0700

    [update] added helper functions to user model and an ip grabber

--- models.py
+++ models.py
@@ -1,16 +1,38 @@
-from store import db
+# from store import db
+from datetime import datetime
+from bcrypt import hashpw, gensalt
 
 
-class User(db.Document):
-    email = db.EmailField(unique=True)
-    password = db.StringField(default=True)
-    active = db.BooleanField(default=True)
-    isAdmin = db.BooleanField(default=False)
-    timestamp = db.DateTimeField(default=datetime.datetime.now())
+class User(object):
+    def __init__(self, email, password):
+        self.email = email  # email will be the username
+        self.password = hashpw(password.encode(
+            'utf-8'), gensalt())  # encrypt password
+        self.timestamp = datetime.now()  # creation time stamp
+        self.cart = []  # empty list of items
+        self.server_ip = None  # pull the ip address of the server
+        self.client_ip = None  # pull the client ip address
+
+    def get_email(self):
+        return self.email
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def json(self):
+        return {
+            'email': self.email,
+            'password': self.password,
+            'created_at': self.timestamp,
+            'server_ip': self.server_ip,
+            'client_ip': self.client_ip
+
+        }
 
 
-class Furniture(db.Document):
-    title = db.StringField(required=True, max_length=120)
-    content = db.StringField()
-    last_updated = db.DateTimeField(default=datetime.datetime.now())
-    user = db.ReferenceField(User)
+class Furniture(object):
+    def __init__(self, src):
+        self.path = path
+
+    def get_path(self):
+        return self.path
