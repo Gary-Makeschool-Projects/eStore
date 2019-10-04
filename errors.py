8b3dab30606d5151f072b9820c60baff66e77298
imthaghost <imthaghost@protonmail.com>
2019-10-04 05:22:33 -0700

    [add] created exceptions with more detailed messages

--- errors.py
+++ errors.py
@@ -3,3 +3,23 @@ import sys
 
 class version_error(Exception):
     pass
+
+
+class session_exipred(Exception):
+    pass
+
+
+class smtp_connection_fail(Exception):
+    pass
+
+
+class credentials_failed(Exception):
+    pass
+
+
+class failed_login(Exception):
+    pass
+
+
+class register_error(Exception):
+    pass
